8c958d36c18f085d82d7764f3edca10046a48957
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 17:35:37 +0700

    Hide application management controls from non-admin users

--- client/ui/pages/applications/page.py
+++ client/ui/pages/applications/page.py
@@ -3,15 +3,7 @@ PAGE_CONFIG = {
     "title": "Applications",
     "endpoint": "/applications",
     "payload_key": "data",
-    "add_button": {"text": "Add Application"},
-    "form": {
-        "fields": {
-            "name": {"type": "text", "label": "Name", "required": True},
-            "description": {"type": "textarea", "label": "Description"},
-        },
-        "submit_text": "Create Application", "edit_submit_text": "Update Application",
-        "clear_button": True, "close_button": True, "popup_width": 360, "maximum_width": 300,
-    },
+    "admin_only": ["add", "edit", "delete"],
     "list": {
         "columns": [
             {"name": "id", "label": "ID"},
