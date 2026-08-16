2d847816f6142f10c1b01d64e2688110dc8586c5
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 14:58:44 +0700

    Simplify app service

--- features/apps/service.py
+++ features/apps/service.py
@@ -1,20 +1,7 @@
-from .repository import AppRepository
+from .repository import app_repository
 
 
-app_repository = AppRepository()
-
-
-def get_apps():
-    return app_repository.get_all()
-
-
-def create_app(data):
-    return app_repository.insert(data)
-
-
-def update_app(app_id, data):
-    return app_repository.update(app_id, data)
-
-
-def delete_app(app_id):
-    return app_repository.delete(app_id)
+def get_apps(): return app_repository.get_all()
+def create_app(data): return app_repository.insert(data)
+def update_app(app_id, data): return app_repository.update(app_id, data)
+def delete_app(app_id): return app_repository.delete(app_id)
