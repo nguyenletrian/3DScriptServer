a7cb76239af08e2bf174132ff12482e6c312a3af
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 14:05:16 +0700

    Make users page a CRUD popup page

--- client/ui/pages/users/page.py
+++ client/ui/pages/users/page.py
@@ -3,8 +3,26 @@ from ...builders.page_builder import PageBuilder
 
 PAGE_CONFIG = {
     "title": "Users",
+    "add_button": {"text": "Add User"},
     "data": {
         "loader": "load",
+        "creator": "create",
+        "updater": "update",
+        "deleter": "delete",
+    },
+    "form": {
+        "popup": True,
+        "fields": {
+            "username": {"type": "text", "label": "Username", "required": True},
+            "password": {"type": "password", "label": "Password"},
+            "role": {"type": "text", "label": "Role", "required": True},
+        },
+        "submit_text": "Create User",
+        "edit_submit_text": "Update User",
+        "clear_button": True,
+        "close_button": True,
+        "popup_width": 360,
+        "maximum_width": 300,
     },
     "list": {
         "columns": [
@@ -12,6 +30,7 @@ PAGE_CONFIG = {
             {"name": "username", "label": "Username"},
             {"name": "role", "label": "Role"},
         ],
+        "actions": ["edit", "delete"],
     },
 }
 
