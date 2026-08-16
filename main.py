a6fa0ef43cfd88c7a7e152d3aa6bdac48a2e7d65
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 15:07:39 +0700

    Use automatic feature router loading

--- main.py
+++ main.py
@@ -4,16 +4,13 @@ from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 
 from core.config import settings
-from features.users.router import router as users_router
-from features.auth.router import router as auth_router
-from features.dashboard.router import router as dashboard_router
-from features.apps.router import router as apps_router
+from core.feature_loader import load_routers
 
 app = FastAPI(title=settings.APP_NAME)
 app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
 templates = Jinja2Templates(directory="templates")
 
-for router in (users_router, auth_router, dashboard_router, apps_router): app.include_router(router)
+for router in load_routers(): app.include_router(router)
 
 
 @app.get("/", response_class=HTMLResponse)
