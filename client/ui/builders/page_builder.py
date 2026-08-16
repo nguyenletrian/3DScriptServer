6084171ee58a08c14c73d77e2f854e6e9b538883
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 18:09:04 +0700

    Make PageBuilder errors visible

--- client/ui/builders/page_builder.py
+++ client/ui/builders/page_builder.py
@@ -111,8 +111,8 @@ class PageBuilder(QWidget):
 
     def set_error(self, message):
         if not hasattr(self, "error_label"):
-            self.error_label = QLabel(); self.error_label.setStyleSheet("color: red; border: none;"); target = self.form_panel or self; layout = target if isinstance(target, QLayout) else target.layout()
-            if layout: layout.addWidget(self.error_label)
+            self.error_label = QLabel(); self.error_label.setStyleSheet("color: red; border: none;")
+            self.layout.addWidget(self.error_label)
         self.error_label.setText(message or "")
 
     def add_item(self):
