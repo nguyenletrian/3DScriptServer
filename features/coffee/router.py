9b4ec5d5c3c278696d928a8da65cc19c0b10a5ef
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 17:21:35 +0700

    Add coffee CRUD routers

---
+++ features/coffee/router.py
@@ -0,0 +1,14 @@
+from core.crud import crud_router
+
+
+def _router(name, collection, prefix):
+    return crud_router({"name": name, "collection": collection, "prefix": prefix, "tag": name.title()})
+
+router = _router("coffee_products", "products", "/coffee/products")
+routers = [
+    router,
+    _router("coffee_categories", "categories", "/coffee/categories"),
+    _router("coffee_customers", "customers", "/coffee/customers"),
+    _router("coffee_orders", "orders", "/coffee/orders"),
+    _router("coffee_order_items", "order_items", "/coffee/order-items"),
+]
