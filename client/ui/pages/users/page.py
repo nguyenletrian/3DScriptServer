b7504d8b2c272357fb6d1a753c21d04fe6509080
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 14:17:42 +0700

    Make users page config-only

--- client/ui/pages/users/page.py
+++ client/ui/pages/users/page.py
@@ -1,15 +1,8 @@
-from ...builders.page_builder import PageBuilder
-
-
 PAGE_CONFIG = {
+    "name": "users",
     "title": "Users",
     "add_button": {"text": "Add User"},
-    "data": {
-        "loader": "load",
-        "creator": "create",
-        "updater": "update",
-        "deleter": "delete",
-    },
+    "data": {"loader": "load", "creator": "create", "updater": "update", "deleter": "delete"},
     "form": {
         "popup": True,
         "fields": {
@@ -17,25 +10,11 @@ PAGE_CONFIG = {
             "password": {"type": "password", "label": "Password"},
             "role": {"type": "text", "label": "Role", "required": True},
         },
-        "submit_text": "Create User",
-        "edit_submit_text": "Update User",
-        "clear_button": True,
-        "close_button": True,
-        "popup_width": 360,
-        "maximum_width": 300,
+        "submit_text": "Create User", "edit_submit_text": "Update User",
+        "clear_button": True, "close_button": True, "popup_width": 360, "maximum_width": 300,
     },
     "list": {
-        "columns": [
-            {"name": "id", "label": "ID"},
-            {"name": "username", "label": "Username"},
-            {"name": "role", "label": "Role"},
-        ],
+        "columns": [{"name": "id", "label": "ID"}, {"name": "username", "label": "Username"}, {"name": "role", "label": "Role"}],
         "actions": ["edit", "delete"],
     },
 }
-
-
-class UsersPage(PageBuilder):
-
-    def __init__(self):
-        super().__init__(PAGE_CONFIG)
