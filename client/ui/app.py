abf2bcb4e7620ab9c697cc0bb5f3b1d0af935390
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 14:49:51 +0700

    Connect app window to config driven navigation

--- client/ui/app.py
+++ client/ui/app.py
@@ -25,33 +25,30 @@ class AppWindow(QWidget):
         self.register_page = RegisterPage()
         self.dashboard_page = DashboardPage()
         self.dynamic_pages = build_pages("apps", "users")
-        self.apps_page = self.dynamic_pages["apps"]
-        self.users_page = self.dynamic_pages["users"]
 
         for page in (self.login_page, self.register_page, self.dashboard_page, *self.dynamic_pages.values()):
             self.pages.addWidget(page)
 
         main_layout = QHBoxLayout(self)
         main_layout.addWidget(self.navigation)
-        main_layout.addWidget(self.pages)
-        self.navigation.login_clicked.connect(lambda: self.show_page(self.login_page))
-        self.navigation.register_clicked.connect(lambda: self.show_page(self.register_page))
-        self.navigation.dashboard_clicked.connect(lambda: self.show_page(self.dashboard_page))
-        self.navigation.apps_clicked.connect(lambda: self.show_page(self.apps_page))
-        self.navigation.users_clicked.connect(lambda: self.show_page(self.users_page))
+        main_layout.addWidget(self.pages, 1)
+        self.navigation.page_clicked.connect(self.show_page)
         self.navigation.logout_clicked.connect(self.logout)
         self.login_page.login_success.connect(self.refresh_ui)
         self.register_page.register_success.connect(self.handle_register_success)
 
-    def show_page(self, page):
-        self.pages.setCurrentWidget(page)
-        if hasattr(page, "on_show"):
-            page.on_show()
+    def show_page(self, name):
+        pages = {"login": self.login_page, "register": self.register_page, "dashboard": self.dashboard_page, **self.dynamic_pages}
+        page = pages.get(name)
+        if page:
+            self.pages.setCurrentWidget(page)
+            if hasattr(page, "on_show"):
+                page.on_show()
 
     def refresh_ui(self):
         user = session.get_user()
         self.navigation.update(user)
-        self.show_page(self.dashboard_page if user else self.login_page)
+        self.show_page("dashboard" if user else "login")
 
     def logout(self):
         api_logout()
@@ -63,4 +60,4 @@ class AppWindow(QWidget):
         self.register_page.password_edit.clear()
         self.register_page.confirm_password_edit.clear()
         self.login_page.username_edit.setText(username)
-        self.show_page(self.login_page)
+        self.show_page("login")
