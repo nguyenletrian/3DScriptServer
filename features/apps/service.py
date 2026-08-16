24f83de5699bce922ea96275df99bcbc1b1163f5
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 14:59:13 +0700

    Remove redundant app service

--- features/apps/service.py
+++
@@ -1,7 +0,0 @@
-from .repository import app_repository
-
-
-def get_apps(): return app_repository.get_all()
-def create_app(data): return app_repository.insert(data)
-def update_app(app_id, data): return app_repository.update(app_id, data)
-def delete_app(app_id): return app_repository.delete(app_id)
