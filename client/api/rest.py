0a76444d8f19160c9928a8e01d645edec15093a2
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 18:03:27 +0700

    Pass application instance through generic RestAPI

--- client/api/rest.py
+++ client/api/rest.py
@@ -5,13 +5,15 @@ from ..session import get_http_session
 
 
 class RestAPI:
-    def __init__(self, endpoint, payload_key=None):
+    def __init__(self, endpoint, payload_key=None, instance_id=None):
         self.url = f"{SERVER_URL}/{endpoint.strip('/')}"
         self.payload_key = payload_key
+        self.instance_id = instance_id
 
     def _request(self, method, path="", data=None):
         payload = {self.payload_key: data} if self.payload_key and data is not None else data
-        response = get_http_session().request(method, f"{self.url}{path}", json=payload, timeout=5)
+        params = {"application_instance_id": self.instance_id} if self.instance_id else None
+        response = get_http_session().request(method, f"{self.url}{path}", params=params, json=payload, timeout=5)
         response.raise_for_status()
         return response.json()
 
