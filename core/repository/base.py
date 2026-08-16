fa59d4b8d71d2b42d1b1511e5fb9cadb36ce13db
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 14:58:21 +0700

    Simplify base repository collections

--- core/repository/base.py
+++ core/repository/base.py
@@ -2,45 +2,10 @@ from core.storage import db
 
 
 class BaseRepository:
-
-    collection = None
-
-    def get(self, item_id):
-        return db.get(
-            self.collection,
-            item_id
-        )
-
-    def get_all(self):
-        return db.get_all(
-            self.collection
-        )
-
-    def insert(self, data):
-        return db.insert(
-            self.collection,
-            data
-        )
-
-    def update(self, item_id, data):
-        return db.update(
-            self.collection,
-            item_id,
-            data
-        )
-
-    def delete(self, item_id):
-        return db.delete(
-            self.collection,
-            item_id
-        )
-    
-    def find_one(self, **conditions):
-        items = self.get_all()
-        for item in items:
-            if all(
-                item.get(key) == value
-                for key, value in conditions.items()
-            ):
-                return item
-        return None
+    def __init__(self, collection): self.collection = collection
+    def get(self, item_id): return db.get(self.collection, item_id)
+    def get_all(self): return db.get_all(self.collection)
+    def insert(self, data): return db.insert(self.collection, data)
+    def update(self, item_id, data): return db.update(self.collection, item_id, data)
+    def delete(self, item_id): return db.delete(self.collection, item_id)
+    def find_one(self, **conditions): return next((item for item in self.get_all() if all(item.get(k) == v for k, v in conditions.items())), None)
