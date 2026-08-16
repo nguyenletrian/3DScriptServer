fc91264be6aa9ea0369c89f59c61b864ff7b9404
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 18:04:17 +0700

    Scope Coffee CRUD to application instances

--- features/coffee/router.py
+++ features/coffee/router.py
@@ -2,7 +2,7 @@ from core.crud import crud_router
 
 
 def _router(name, collection, prefix):
-    return crud_router({"name": name, "collection": collection, "prefix": prefix, "tag": name.title()})
+    return crud_router({"name": name, "collection": collection, "prefix": prefix, "tag": name.title(), "instance_scoped": True, "write_admin_only": False})
 
 router = _router("coffee_products", "products", "/coffee/products")
 routers = [
