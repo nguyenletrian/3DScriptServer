c0a0ecc0e6d3b31f4f6bee8a954398aeff2988ab
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-08-16 14:58:26 +0700

    Use generic user repository

--- features/users/repository.py
+++ features/users/repository.py
@@ -1,20 +1,4 @@
 from core.repository import BaseRepository
 
 
-class UserRepository(BaseRepository):
-
-    collection = "users"
-
-    def find_by_username(self, username):
-
-        users = self.get_all()
-
-        for user in users:
-
-            if user["username"] == username:
-                return user
-
-        return None
-
-
-user_repository = UserRepository()
+user_repository = BaseRepository("users")
